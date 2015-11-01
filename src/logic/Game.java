962a48337495bb80d1b7c70550dc455d33f0730a
Mortenbaws <morten2094@hotmail.com>
2015-11-01 10:32:51 +0100

    Created all classes(empty) and made base Field class

---
+++ src/logic/Game.java
@@ -0,0 +1,15 @@
+package logic;
+
+/**
+ * Date: 01/11/2015
+ *
+ * Project: CDIO2
+ *
+ * File: Game.java
+ *
+ * Created by: Morten Jørvad
+ */
+
+public class Game {
+
+}
---
+++ src/run/GUI.java
@@ -0,0 +1,15 @@
+package run;
+
+/**
+ * Date: 01/11/2015
+ *
+ * Project: CDIO2
+ *
+ * File: GUI.java
+ *
+ * Created by: Morten Jørvad
+ */
+
+public class GUI {
+
+}
